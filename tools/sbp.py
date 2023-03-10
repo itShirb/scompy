d2d650faa65cea67b4ffcd0a75174dbeee26ca16
itShirb <69372252+itShirb@users.noreply.github.com>
2023-03-09 21:12:37 -0500

    Update sbp.py

--- tools/sbp.py
+++ tools/sbp.py
@@ -1,7 +1,7 @@
 import os
 print("Where is the binary located. Make sure to include the name of the binary. (ex: /bin/executable.exe)\n")
 path = input(">> ")
-f = open("./tools/binpath", "a")
+f = open("./tools/binpath", "w")
 f.write(path)
 f.close()
 print("Successfully saved binary path as {}".format(path))
